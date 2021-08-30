46fb5eab32badea76139492afe662c4917e83f7e
Juan Campos <juancampos.bsi@outlook.com>
2021-08-30 11:11:43 -0300

    Update master

---
+++ src/pages/home.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react';
+import { View, Text } from 'react-native';
+
+export default function home() {
+  return (
+    <>     
+     <View style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <Text> React Native Hoje 30/08/2021 </Text>
+      </View>
+
+      <Text style={{
+        alignSelf: 'center',
+        marginBottom: 20
+      }}>
+        Ignite
+      </Text>
+    </>
+  )
+}
